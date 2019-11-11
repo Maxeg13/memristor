b4270c7a35de11930a6041593a2a6ab5d218240b
Dr Pepper <chibisaincrada@gmail.com>
2019-11-11 21:20:16 +0300

    avr_file

--- memristor/main.c
+++ memristor/main.c
@@ -20,8 +20,21 @@
 
 #define BAUD 9600                                   // define baud
 #define BAUDRATE ((F_CPU)/(BAUD*2*16UL)-1)//16UL
+
+
+
+typedef enum
+{
+	CUSTOM,
+	VAC,
+	PROGRAM
+} MODE;
+
+MODE MD=CUSTOM;
+//MODE MD=CUSTOM;
 //#define DD_SS PB0
-int16_t VAC16=0;
+int16_t VAC16=0, VAC16_H=0, VAC16_HH=0;
+int16_t prog_val=0;
 int16_t x16=0;
 int16_t y16=0;
 uint8_t sync=0;
@@ -30,23 +43,28 @@ uint8_t t2=2;
 uint8_t dTt2=10;
 uint8_t dT;
 uint8_t T;
+uint8_t pos_phase=1;
 uint8_t send8;
 uint8_t ptr=0, UDP_cnt;
+uint8_t PROGRAM_done=0;
 char c;
 uint8_t ADC_on;
-uint8_t VAC_mode=0;
+//uint8_t VAC_mode=0;
 uint8_t _adc;
 int ctr;
+
 uint16_t accum;
 int event_ctr;
-int time_step=3;//3
+int time_step=6;//3
 int eventN=100;
 int ADC_cnt;
+uint8_t ADCH_, ADCL_;
 uint8_t accum_cnt;
 
 /* Функция инициализация АЦП */
 void ADC_Init(){
  ADCSRA |= (1 << ADEN) // Включаем АЦП
+ //ADCSRA&=~(1 << ADPS1)|(1 << ADPS0)|(1 << ADPS1);
  |(1 << ADPS1)|(1 << ADPS0)|(1 << ADPS1);    // устанавливаем предделитель преобразователя на 8
  ADMUX |= (0 << REFS1)|(1 << REFS0) //выставляем опорное напряжение, как внешний ИОН
  |(0 << MUX0)|(0 << MUX1)|(0 << MUX2)|(0 << MUX3); // снимать сигнал будем с  входа PC0 
@@ -83,7 +101,7 @@ void SPI_MasterInit()
 	//SPSR = (0<<SPI2X);
 }
 //(1<<RXCIE0)|
-void setDAC(int16_t x)//_____________bipolar!!!
+void setDAC(int16_t x)//_____________bipolar!!! and <<4 larger
 {
 	x=-x;
 	x+=2048;
@@ -155,10 +173,9 @@ void main(void)
 
 ISR(TIMER2_OVF_vect)
 {
-
 	if(ctr>time_step)
 	{
-		if(!VAC_mode)
+		if(MD==CUSTOM)
 		{
 			//if(ADC_cnt==30)
 			//{
@@ -171,13 +188,13 @@ ISR(TIMER2_OVF_vect)
 			ADC_on=1;			
 			_adc=((ADCL>>2)|(ADCH <<6));
 			//ADMUX|=(1<<MUX0);
-			ADCSRA |= (1 << ADSC); 
+			
 			}
-			else if(ADC_on)
-			{
-				accum_cnt++;
-				accum+=_adc;
-			}
+			//else if(ADC_on)
+			//{
+			//	accum_cnt++;
+			//	accum+=_adc;
+			//}
 			
 			
 			
@@ -205,6 +222,8 @@ ISR(TIMER2_OVF_vect)
 			PORTD|=(1<<LDAC);
 		
 			}
+			else if(event_ctr==(dT+1))
+				ADCSRA |= (1 << ADSC); 
 
 			else if(event_ctr==dTt2)
 			{
@@ -225,58 +244,141 @@ ISR(TIMER2_OVF_vect)
 			}		
 
 		}
-		else//VAC_mode
-		{
-			//UDR0=255;
+		else if(MD==VAC)//VAC_mode
+		{			
 			static int i=0;
-			i++;
+			i++;						
+			
 			switch(UDP_cnt)
 			{
 				case 0:
+					ADCSRA |= (1 << ADSC); 
+				 //important  ADC - DAC delay
 				UDR0=255;
-				ADCSRA |= (1 << ADSC); 
 				break;
-				case 1:
-				_adc=((ADCL>>2)|(ADCH <<6));
+				
+				
+				
+				case 1:		
+				_adc=(ADCL>>2)|(ADCH<<6);//old
 				UDR0=_adc;
 				break;
+				
+				//case 2:
+				//UDR0=ADCH;
+				//break;
+				
+				
 				case 2:
-				UDR0=VAC16>>4;	
+				UDR0=VAC16>>4;				
+				//VAC16_HH=VAC16_H;
+				//VAC16_H=VAC16;
 				
-				
-				VAC16+=16;
-				if(VAC16>y16)//just positive
+				if(pos_phase)
 				{
-					//VAC16=x16|0xFC00;
-					VAC16=-x16;
-					//VAC16=-VAC16;
+					VAC16+=16;//16
+					if(VAC16>y16)//just positive
+					{
+					pos_phase=0;
+					//VAC16=-x16;				
+					}
 				}
-					
-				setDAC(VAC16);			
+				else
+				{
+					VAC16-=16;
+					if(VAC16<-x16)//just positive
+					{
+					pos_phase=1;
+					//VAC16=-x16;				
+					}
+				}	
+				
+				
+				setDAC(VAC16);	
 				PORTD&=~(1<<LDAC);
 				PORTD|=(1<<LDAC);
 				
 			}
-			
-//old version of _adc transfer			
-			
-			//_adc
-			
-			
-			
-			
-
-			
-			
-
-				//VAC16=-y16;//just positive
-			
+						
 			
 			
 			UDP_cnt++;
 			UDP_cnt%=3;
-			//UDR0=VAC16>>4;
+
 			
+		}
+		else if(MD==PROGRAM)
+		{
+			static uint16_t adc_h;
+			//static int8_t mul=1;
+			if(event_ctr==(1))//ADC!!!
+			{	
+			UDR0=PROGRAM_done;
+			//ADC_on=1;	
+			adc_h=((255-_adc)<<4);	
+			_adc=((ADCL>>2)|(ADCH <<6));
+				if((adc_h<(x16+(2<<4)))&&(adc_h>(x16-(2<<4))))
+				{
+					PROGRAM_done=1;
+					prog_val=0;
+				}
+			//ADMUX|=(1<<MUX0);
+			
+			}
+
+			
+			if(event_ctr==0)
+			{
+				UDR0=255;
+				prog_val-=16;
+				if(prog_val==-2032)
+					prog_val=2016;
+				else
+				if(prog_val<0)
+					prog_val=-2016;
+				
+				if(PROGRAM_done)
+					prog_val=0;
+			setDAC(prog_val);
+			PORTD&=~(1<<LDAC);
+			PORTD|=(1<<LDAC);
+			}
+			
+			else if(event_ctr==t1)
+			{
+			//if ( ( UCSR0A & (1<<UDRE0)) )		
+			//UDR0=ADCL;	
+				
+			setDAC(0);
+			PORTD&=~(1<<LDAC);
+			PORTD|=(1<<LDAC);
+			}
+			else if(event_ctr==dT)
+			{		
+			setDAC(y16);
+			PORTD&=~(1<<LDAC);
+			PORTD|=(1<<LDAC);
+		
+			}
+			else if(event_ctr==(dT+1))
+				ADCSRA |= (1 << ADSC); 
+
+			else if(event_ctr==dTt2)
+			{
+			//if ( ( UCSR0A & (1<<UDRE0)) )			
+				UDR0=_adc;	
+			//UDR0=(uint8_t)(accum/t2);
+			accum=0;
+			ADC_on=0;
+			accum_cnt=0;
+			//if ( ( UCSR0A & (1<<UDRE0)) )		
+			//UDR0=ADCH;	
+			//ADCSRA |= (1 << ADSC); 			
+			setDAC(0);
+			PORTD&=~(1<<LDAC);
+			PORTD|=(1<<LDAC);
+			//ADMUX&=~(1<<MUX0);
+			}
 		}
 		
 		
@@ -285,7 +387,7 @@ ISR(TIMER2_OVF_vect)
 		
 		ctr=0;
 		event_ctr++;
-		ADC_cnt++;
+		//ADC_cnt++;
 		if(event_ctr>T)
 			event_ctr=0;
 	}
@@ -309,11 +411,18 @@ ISR(USART_RX_vect)
 			sync=1;
 		break;
 		case 1:
-		VAC_mode=UDR0;
-		if(VAC_mode)
-			time_step=7;
+		MD=UDR0;
+		if(MD==VAC)
+			time_step=4;//5
 		else
-			time_step=4;
+			time_step=4;//4
+		
+		if(MD==PROGRAM)
+		{
+			//PROGRAM_start=1;
+			PROGRAM_done=0;
+			prog_val=-3;
+		}
 		break;
 		case 2:
 		x16=UDR0<<4;
